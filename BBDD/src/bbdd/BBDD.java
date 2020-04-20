cb7c22c44b1b8549018d232d63675a8a1d2593a5
Ria Rabanales <mrabanales@cifpfbmoll.eu>
2020-04-20 19:45:32 +0200

    Update inicial

---
+++ BBDD/src/bbdd/BBDD.java
@@ -0,0 +1,12 @@
+/*
+ * Maria Rabanales González
+ */
+package bbdd;
+
+public class BBDD {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
